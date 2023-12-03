08ed8aedcf728460305f95f72f81045b9e04cdb7
Penhoian <dongbanbr002@gmail.com>
2023-12-03 18:27:17 +0700

    codingChallenge1

---
+++ codingChallenge1.js
@@ -0,0 +1,40 @@
+const eleContainer = document.querySelector('.container');
+const eleCheck = document.querySelector('.check');
+const eleAgain = document.querySelector('.again');
+const eleScore = document.querySelector('#score');
+const eleStatus = document.querySelector('.status');
+const eleNumber = document.querySelector('.number');
+const eleHightScore = document.querySelector('#hightScore');
+let score = 20;
+let maxScore = [0];
+eleScore.innerHTML = `${score}`;
+eleHightScore.innerHTML = `${Math.max(...maxScore)}`;
+
+let randomNumber = 9;
+eleAgain.addEventListener('click', function(){
+    eleContainer.style.backgroundColor = 'white';
+    randomNumber=Math.floor(20*Math.random());
+    document.querySelector('.box input[type="number"]').value= '';
+    eleNumber.innerHTML =`?`;
+    eleStatus.innerHTML =`Start guessing...`
+    score = 20;
+    eleScore.innerHTML = `${score}`;
+    eleHightScore.innerHTML = `${Math.max(...maxScore)}`
+})
+eleCheck.addEventListener('click', function(){
+    const eleBoxVal = document.querySelector('.box input[type="number"]').value;
+    console.log(randomNumber);
+    console.log(eleBoxVal);
+    if(Number(eleBoxVal) === randomNumber){
+        eleStatus.innerHTML =`Correct`;
+        maxScore.push(score);
+        eleContainer.style.backgroundColor = 'green';
+        eleNumber.innerHTML = `${randomNumber}`
+    }else{
+        score --;
+        eleContainer.style.backgroundColor = 'white';
+        eleScore.innerHTML = `${score}`;
+        (eleBoxVal > randomNumber)? (eleStatus.innerHTML =`Too Hight`) : (eleStatus.innerHTML =`Too Low`);
+    }
+})
+
